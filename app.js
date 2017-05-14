4082160d9b83e807678f7196e222f0612fd8e6f9
tanujap148 <tanujap148@gmail.com>
2017-05-15 01:14:42 +0530

    session5assign2-code

---
+++ app.js
@@ -0,0 +1,8 @@
+/*storing the details of a person in the object named person */
+var person = {
+name : "Ramesh",
+age : 50,
+dateofbirth : "03/03/1967",
+placeofbirth : "Bhopal",
+};
+document.getElementById("details").innerHTML= "Name: "+person.name+"<br>Age: "+person.age+"<br>Date Of Birth: "+person.dateofbirth+"<br>Place of birth: "+person.placeofbirth; //now displaying the details
